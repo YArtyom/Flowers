bb9c725bad622b6d027e038a58a6db5380041323
Shuxa <165983146+MonkSherk@users.noreply.github.com>
2024-10-08 22:10:41 +0500

    Update crud.py
    Changed user_create func

--- api/crud.py
+++ api/crud.py
@@ -1,64 +1,77 @@
-from sqlalchemy.orm import Session
-from models import Customer, Product, Basket, BasketItem
-from schemas import CustomerCreate, BasketItemOut
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from models import User, Product, Basket, BasketItem
+from schemas import BasketItemOut
 from auth import get_password_hash
 from fastapi import HTTPException, status
+from user.schema import uAuth
 
 
-def create_customer(db: Session, customer: CustomerCreate):
-    hashed_password = get_password_hash(customer.password)
-    db_customer = Customer(name=customer.name, mail=customer.mail, password=hashed_password)
-    db.add(db_customer)
-    db.commit()
-    db.refresh(db_customer)
-    return db_customer
+# Создание пользователя
+async def create_customer(db: AsyncSession, customer: uAuth):
+    db_user = User(name=customer.name, mail=customer.mail, hashed_password=customer.password)
+    db.add(db_user)
+    await db.commit()
+    await db.refresh(db_user)
+    return db_user
 
 
-def get_products(db: Session):
-    return db.query(Product).all()
+
+# Получение списка продуктов
+async def get_products(db: AsyncSession):
+    result = await db.execute(select(Product))
+    return result.scalars().all()
 
 
 # CRUD для корзины
-def get_active_basket(db: Session, customer_id: int):
-    return db.query(Basket).filter(Basket.customer_id == customer_id, Basket.active_status == True).first()
+async def get_active_basket(db: AsyncSession, customer_id: int):
+    result = await db.execute(select(Basket).filter(Basket.customer_id == customer_id, Basket.active_status == True))
+    return result.scalar()
 
 
-def create_basket(db: Session, customer_id: int):
+async def create_basket(db: AsyncSession, customer_id: int):
     basket = Basket(customer_id=customer_id, price=0, active_status=True)
     db.add(basket)
-    db.commit()
-    db.refresh(basket)
+    await db.commit()
+    await db.refresh(basket)
     return basket
 
 
-def add_item_to_basket(db: Session, basket: Basket, product_id: int, quantity: int):
-    product = db.query(Product).filter(Product.id == product_id).first()
+async def add_item_to_basket(db: AsyncSession, basket: Basket, product_id: int, quantity: int):
+    result = await db.execute(select(Product).filter(Product.id == product_id))
+    product = result.scalar()
+
     if not product:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
+
     if product.quantity < quantity:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Not enough quantity in stock")
+
     basket_item = BasketItem(basket_id=basket.id, product_id=product_id, price=product.price, quantity=quantity)
     db.add(basket_item)
     basket.price += product.price * quantity
-    db.commit()
-    db.refresh(basket)
+    await db.commit()
+    await db.refresh(basket_item)
 
     return basket_item
 
 
-def get_basket_items(db: Session, basket_id: int):
-    return db.query(BasketItem).filter(BasketItem.basket_id == basket_id).all()
+async def get_basket_items(db: AsyncSession, basket_id: int):
+    result = await db.execute(select(BasketItem).filter(BasketItem.basket_id == basket_id))
+    return result.scalars().all()
 
 
-def remove_item_from_basket(db: Session, basket_item_id: int):
-    item = db.query(BasketItem).filter(BasketItem.id == basket_item_id).first()
+async def remove_item_from_basket(db: AsyncSession, basket_item_id: int):
+    result = await db.execute(select(BasketItem).filter(BasketItem.id == basket_item_id))
+    item = result.scalar()
+
     if not item:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Item not found")
 
     basket = item.basket
     basket.price -= item.price * item.quantity
 
-    db.delete(item)
-    db.commit()
+    await db.delete(item)
+    await db.commit()
 
     return item

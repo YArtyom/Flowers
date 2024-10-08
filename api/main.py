a5a213744626dd0622f743bfb0e909f74088998b
Shuxa <165983146+MonkSherk@users.noreply.github.com>
2024-10-08 22:09:15 +0500

    Update main.py
    Added router for templates
    Added auth user routers

--- api/main.py
+++ api/main.py
@@ -1,50 +1,49 @@
 import shutil
 from datetime import timedelta
 from typing import List
-from fastapi import FastAPI, Depends, HTTPException, status, UploadFile, File
+from fastapi import FastAPI, Depends, HTTPException, UploadFile, File
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from sqlalchemy.ext.asyncio import AsyncSession
 from auth import create_access_token, authenticate_user, get_current_user, ACCESS_TOKEN_EXPIRE_MINUTES
 from crud import create_customer, get_products, get_active_basket, create_basket, add_item_to_basket, get_basket_items, \
     remove_item_from_basket
-from schemas import BasketItemOut, BasketOut, ProductOut, CustomerOut, CustomerCreate
-from database import engine, Base, get_db, init_db
-from pages.router import routerS as rout_pages
+from pages import basket, profile, home , auth
+from schemas import BasketItemOut, BasketOut, ProductOut, CustomerOut
+from database import async_session_maker, engine, Base, get_db
 from fastapi.staticfiles import StaticFiles
+from user.router import router as user_router
+from user.schema import uAuth, Token
+
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Темплейты
+app.include_router(home.router)
+app.include_router(profile.router)
+app.include_router(basket.router)
+app.include_router(auth.router)
+
+# регистрация пользователя
+app.include_router(user_router)
+
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-app.include_router(rout_pages)
+
+
 
 @app.on_event("startup")
 async def on_startup():
-    await init_db()
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
 
-@app.post("/register", response_model=CustomerOut)
-async def register(customer: CustomerCreate, db: AsyncSession = Depends(get_db)):
-    return await create_customer(db, customer)
-
-
-@app.post("/token")
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)):
-    user = await authenticate_user(db, form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect email or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(data={"sub": user.mail}, expires_delta=access_token_expires)
-    return {"access_token": access_token, "token_type": "bearer"}
 
 @app.get("/products", response_model=List[ProductOut])
-async def get_products(db: AsyncSession = Depends(get_db)):
+async def get_products_endpoint(db: AsyncSession = Depends(get_db)):
     return await get_products(db)
 
+
 @app.get("/basket", response_model=BasketOut)
-async def get_basket(db: AsyncSession = Depends(get_db), token: str = Depends(oauth2_scheme)):
+async def get_basket(db: AsyncSession = Depends(get_db), token: str = Depends(uAuth)):
     current_user = await get_current_user(token, db)
     basket = await get_active_basket(db, current_user.id)
     if not basket:
@@ -52,6 +51,7 @@ async def get_basket(db: AsyncSession = Depends(get_db), token: str = Depends(oa
     items = await get_basket_items(db, basket.id)
     return {"id": basket.id, "customer_id": basket.customer_id, "price": basket.price,
             "active_status": basket.active_status, "items": items}
+
 
 @app.post("/basket/add", response_model=BasketItemOut)
 async def add_to_basket(product_id: int, quantity: int, db: AsyncSession = Depends(get_db), token: str = Depends(oauth2_scheme)):
@@ -62,11 +62,13 @@ async def add_to_basket(product_id: int, quantity: int, db: AsyncSession = Depen
     basket_item = await add_item_to_basket(db, basket, product_id, quantity)
     return basket_item
 
+
 @app.delete("/basket/remove/{basket_item_id}", response_model=BasketItemOut)
 async def remove_from_basket(basket_item_id: int, db: AsyncSession = Depends(get_db), token: str = Depends(oauth2_scheme)):
     await get_current_user(token, db)
     basket_item = await remove_item_from_basket(db, basket_item_id)
     return basket_item
+
 
 @app.post("/upload-photo/")
 async def upload_photo(file: UploadFile = File(...)):
